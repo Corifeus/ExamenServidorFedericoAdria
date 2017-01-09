6cfadf59a1fd2478f46ba2a77395f5c75aa744a6
Federico Adria <transylv@gmail.com>
2017-01-09 19:36:59 +0100

    Error con la configuración

---
+++ src/gestorBundle/Controller/usuariosController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace gestorBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use gestorBundle\Entity\User;
+use gestorBundle\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class usuariosController extends Controller
+{
+
+    public function usuariosAction()
+    {
+        return $this->render('gestorBundle:usuarios:index.html.twig');
+    }
+
+    public function loginAction()
+    {
+      $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('gestorBundle:usuarios:login.html.twig', array(
+        'last_username' => $lastUsername,
+        'error'         => $error,
+      ));
+    }
+}
